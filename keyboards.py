5ece23b503cb638cdc8122e6e048cf1e2a9d476b
Федор Иванов <tehnik357.copy@yandex.ru>
2019-07-06 21:26:38 +0300

    Изменил имена переменных

--- keyboards.py
+++ keyboards.py
@@ -1,10 +1,10 @@
 import telebot
 
 
-main_keyboard = telebot.types.ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True, row_width=10)
-main_keyboard.row('Погода', 'Прогноз')
-main_keyboard.row('Помощь')
+main = telebot.types.ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True, row_width=10)
+main.row('Погода', 'Прогноз')
+main.row('Помощь')
 
 
-error_keyboard = telebot.types.ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=False)
-error_keyboard.row('Задать город', 'Назад')
+error = telebot.types.ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=False)
+error.row('Задать город', 'Назад')
