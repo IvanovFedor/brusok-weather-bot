039cf8f8d4ddff0875f31a7c70d26390b9191b6e
josephRTi <josephrtix@gmail.com>
2019-07-06 10:16:45 +0300

    Update keyboards.py

--- keyboards.py
+++ keyboards.py
@@ -3,6 +3,7 @@ import telebot
 
 main_keyboard = telebot.types.ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True, row_width=10)
 main_keyboard.row('Погода', 'Прогноз')
+main_keyboard.row('Помощь')
 
 
 error_keyboard = telebot.types.ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=False)
