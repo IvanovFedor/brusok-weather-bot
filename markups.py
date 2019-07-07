c3df86b95fd72251f1cac5d818ca30dbe973b29f
josephRTi <josephrtix@gmail.com>
2019-07-07 12:48:44 +0300

    Update markups.py

--- markups.py
+++ markups.py
@@ -7,5 +7,5 @@ main.row('Помощь \U00002753')
 
 
 error = telebot.types.ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=False)
-error.row('Задать город', 'Назад')
+error.row('Задать город \U0001F4CD', 'Назад \U0001F519')
 
