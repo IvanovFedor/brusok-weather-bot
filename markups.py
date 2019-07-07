cbcbf967064c5a5747a183e13f84f60e1605a94a
Федор Иванов <tehnik357.copy@yandex.ru>
2019-07-07 13:02:13 +0300

    Update markups and bot

--- markups.py
+++ markups.py
@@ -1,11 +1,11 @@
-import telebot
+from telebot import types
 
 
-main = telebot.types.ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True, row_width=10)
+main = types.ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True, row_width=10)
 main.row('Погода \U000026C5', 'Прогноз \U0001F4C5')
 main.row('Помощь \U00002753')
 
 
-error = telebot.types.ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=False)
+error = types.ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=False)
 error.row('Задать город \U0001F4CD', 'Назад \U0001F519')
 
