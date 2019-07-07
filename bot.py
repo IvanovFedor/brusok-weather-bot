e7238f6676d4230a5c8f463d8f3414befa51f424
Федор Иванов <tehnik357.copy@yandex.ru>
2019-07-07 12:45:05 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,16 +1,24 @@
 import telebot
 from json import load
 import markups
-import telegramfont as font
 
 apikeys = load(open('apikeys.json', 'r'))
+
+templates = {'info': '''
+Интересует погода?
+Просто отправь мне одну из этих команд:
+
+*Погода* \U000026C5 на текущую дату и время
+*Прогноз* \U0001F4C5 на следующие 3 дня
+*Помощь* \U00002753
+                     '''}
 
 bot = telebot.TeleBot(apikeys['TeleBot'])
 
 
 @bot.message_handler(commands=['start', 'help'])
 def info_action(msg):
-    bot.send_message(msg.from_user.id, font.bold("Текст с помощью"),
+    bot.send_message(msg.from_user.id, templates['info'],
                      reply_markup=markups.main, parse_mode='MarkDown')
 
 
